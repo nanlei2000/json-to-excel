532b5b8ad3b5e4015fc9b207928f49c98b7ead65
nanlei <36439285+nanlei2000@users.noreply.github.com>
2019-12-08 16:19:01 +0800

    feat: sql辅助函数

--- be/script/Idiom.ts
+++ be/script/Idiom.ts
@@ -13,24 +13,25 @@ export interface Item {
 }
 import mysql from 'mysql';
 import fs from 'fs-extra';
-import { escape } from '../src/shared'
+import { fieldHelper } from '../src/shared'
+import { IdiomTable } from '../src/database/mysql'
 export const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: 'kh152572515',
     database: 'fun_api'
 });
-
+const field = fieldHelper<IdiomTable>();
 function setRow(row: Item): Promise<unknown> {
     return new Promise((resolve, reject) => {
         connection.query(
-            escape`INSERT INTO idiom SET
-            derivation=${row.derivation},
-            example=${row.example},
-            explanation=${row.explanation},
-            pinyin=${row.pinyin},
-            word=${row.word},
-            abbr=${row.abbreviation}
+            `INSERT INTO idiom SET
+            ${field("derivation", row.derivation)},
+            ${field("example", row.example)},
+            ${field("explanation", row.explanation)},
+            ${field("pinyin", row.pinyin)},
+            ${field("word", row.word)},
+            ${field("abbr", row.abbreviation)}
         `, (error) => {
             if (error) { reject(error); }
             resolve()
@@ -42,10 +43,11 @@ const json: Item[] = fs.readJSONSync('./idiom.json');
 Promise.all(json.map(v => setRow(v)))
     .then(() => {
         console.log('成功')
-    }).catch((err) => {
+    })
+    .catch((err) => {
         console.log(err.message);
     }
-    )
-
-
-
+    ).finally(() => {
+        connection.destroy();
+        process.exit(0);
+    });
--- be/src/shared/Misc.ts
+++ be/src/shared/Misc.ts
@@ -15,8 +15,14 @@ export const getRandomInt = () => {
 export const currentUrl = `http://${process.env.HOST}:${process.env.PORT}`;
 
 export function escape(strings: TemplateStringsArray, ...values: any[]): string {
-
     return strings.map((s, index) =>
         (index < values.length ? `${s}${mysql.escape(values[index])}` : s))
         .join('')
+};
+
+export function fieldHelper<T extends object>() {
+    return function (
+        key: Extract<keyof T, string>, value: T[Extract<keyof T, string>]): string {
+        return `${key}=${mysql.escape(value)}`
+    };
 };
