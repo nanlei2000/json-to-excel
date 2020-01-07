5875f719842a4c369704cd6eb0f1c1bd185ee666
nanlei2000 <1456858184@qq.com>
2020-01-07 16:55:15 +0800

    feat: 添加`/api/idiom//get-random-chain`接口

--- be/src/daos/Idiom/IdiomDao.ts
+++ be/src/daos/Idiom/IdiomDao.ts
@@ -1,4 +1,5 @@
 import { connection, IdiomTable } from '../../database/mysql';
+import _ from 'lodash';
 const field = IdiomTable.field;
 const fieldV = IdiomTable.fieldV;
 
@@ -8,11 +9,37 @@ export class IdiomDao {
         return new Promise((resolve, reject) => {
             const sql = `SELECT ${field("word")} from ${IdiomTable.name}
             WHERE ${field("word")} REGEXP '^${word[word.length - 1]}'
-            `
+            `;
             connection.query(sql, (err, result: Pick<IdiomTable.Fields, 'word'>[]) => {
-                if (err) { reject(err) };
+                if (err) { reject(err); };
                 resolve(result);
-            })
-        })
+            });
+        });
     }
+    /** @throws IOException */
+    public async getRandomChain(word: string): Promise<string[]> {
+        const allWords = await new Promise<Pick<IdiomTable.Fields, 'word'>[]>((resolve, reject) => {
+            const allWordsSql = `SELECT ${field("word")} FROM ${IdiomTable.name}
+            WHERE ${field('id')} > 0
+            `;
+            connection.query(allWordsSql, (err, result: Pick<IdiomTable.Fields, 'word'>[]) => {
+                if (err) { reject(err); };
+                resolve(result);
+            });
+        });
+        return findRandomChain(word, allWords.map(v => v.word));
+    }
+}
+
+function findRandomChain(head: string, words: string[]): string[] {
+    function loop(head_: string, chain: string[]): string[] {
+        const nextWords = words.filter(v => v[0] === head_[head_.length - 1] && !chain.includes(v));
+        if (!nextWords.length) {
+            return chain;
+        } else {
+            const randomHead = nextWords[_.random(0, nextWords.length - 1)];
+            return loop(randomHead, [...chain, randomHead]);
+        }
+    }
+    return loop(head, []);
 }
--- be/src/routes/Idiom.ts
+++ be/src/routes/Idiom.ts
@@ -13,9 +13,9 @@ const idiomDao = new IdiomDao();
 namespace GetAll {
     export const path = '/get-all';
     interface ResData {
-        words: string[]
+        words: string[];
     }
-    const wordReg: RegExp = /^[\u4e00-\u9fa5]{4,}$/;
+    const wordReg: RegExp = /^[\u4e00-\u9fa5]{1,}$/;
     type Query = Record<'word', string | undefined>;
     export async function coreMid(
         req: Request,
@@ -31,12 +31,50 @@ namespace GetAll {
                         words: rows.map(v => v.word)
                     },
                     msg: "查询成功"
-                })
+                });
             } else {
                 return res.json({
                     code: BAD_REQUEST,
-                    msg: "无效的成语"
-                })
+                    msg: "无效的参数"
+                });
+            }
+
+        } catch (err) {
+            logger.error(err.message, err);
+            return res.json({
+                msg: err.message,
+                code: BAD_REQUEST,
+            });
+        }
+    }
+}
+namespace GetLongest {
+    export const path = '/get-random-chain';
+    interface ResData {
+        words: string[];
+    }
+    const wordReg: RegExp = /^[\u4e00-\u9fa5]{1,}$/;
+    type Query = Record<'word', string | undefined>;
+    export async function coreMid(
+        req: Request,
+        res: Res<ResData>
+    ): Promise<Res<ResData>> {
+        try {
+            const { word } = req.query as Query;
+            if (word && wordReg.test(word)) {
+                const rows = await idiomDao.getRandomChain(word);
+                return res.json({
+                    code: 200,
+                    data: {
+                        words: rows
+                    },
+                    msg: "查询成功"
+                });
+            } else {
+                return res.json({
+                    code: BAD_REQUEST,
+                    msg: "无效的参数"
+                });
             }
 
         } catch (err) {
@@ -50,4 +88,5 @@ namespace GetAll {
 }
 
 router.get(GetAll.path, GetAll.coreMid);
+router.get(GetLongest.path, GetLongest.coreMid);
 export const IdiomRouter = router;
