23380a372685a439a5b5f63cc2759891bda1e430
nanlei <36439285+nanlei2000@users.noreply.github.com>
2020-01-15 17:19:52 +0800

    fix: 修改sql

--- be/script/sql/db.sql
+++ be/script/sql/db.sql
@@ -2,13 +2,15 @@
 CREATE SCHEMA `fun_api` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 
 -- 成语表
-CREATE TABLE `fun_api`.`idiom` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `word` VARCHAR(100) NOT NULL DEFAULT '\"\"' COMMENT '成语',
-  `pinyin` VARCHAR(100) NOT NULL DEFAULT '\"\"' COMMENT '拼音',
-  `abbr` VARCHAR(45) NOT NULL DEFAULT '\"\"' COMMENT '拼音缩写',
-  `derivation` VARCHAR(2000) NOT NULL DEFAULT '\"\"' COMMENT '出处',
-  `example` VARCHAR(2000) NOT NULL DEFAULT '\"\"' COMMENT '示例',
-  `explanation` VARCHAR(2000) NOT NULL DEFAULT '\"\"' COMMENT '释义',
+CREATE TABLE `idiom` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `word` varchar(100) NOT NULL DEFAULT '""' COMMENT '成语',
+  `pinyin` varchar(100) NOT NULL DEFAULT '""' COMMENT '拼音',
+  `abbr` varchar(45) NOT NULL DEFAULT '""' COMMENT '拼音缩写',
+  `derivation` varchar(2000) NOT NULL DEFAULT '""' COMMENT '出处',
+  `example` varchar(2000) NOT NULL DEFAULT '""' COMMENT '示例',
+  `explanation` varchar(2000) NOT NULL DEFAULT '""' COMMENT '释义',
+  `next_id_str` varchar(10000) CHARACTER SET utf8mb4 DEFAULT '""',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30896 DEFAULT CHARSET=utf8;
